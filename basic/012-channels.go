b29a628ce5921b7e5973f3765a534c59b8769c3e
Lenin <lenin@pathao.com>
2017-09-26 11:38:56 +0600

    testing channels

---
+++ basic/012-channels.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	// done := make(chan string)
+	// go func() { done <- "true" }()
+
+	done := make(chan string, 1)
+	// done := make(chan string)
+	done <- "true"
+
+	s := <-done
+	fmt.Println("hello", s)
+}

5a5eeddfbd4daba7007604c6e17f612e1e22148c
Lenin <lenin@pathao.com>
2017-10-04 19:19:34 +0600

    reading file

---
+++ basic/041-file.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+var p = fmt.Print
+var pln = fmt.Println
+var pf = fmt.Printf
+
+func main() {
+	file := "/tmp/defer.txt"
+
+	dat, err := ioutil.ReadFile(file)
+	check(err)
+	p(string(dat))
+
+	f, err := os.Open(file)
+	defer f.Close()
+	check(err)
+
+	b1 := make([]byte, 1)
+	n1, err := f.Read(b1)
+	check(err)
+	pf("%d bytes: %s\n", n1, string(b1))
+
+	o2, err := f.Seek(2, 0)
+	check(err)
+	b2 := make([]byte, 2)
+	n2, err := f.Read(b2)
+	check(err)
+	pf("%d bytes @ %d: %s\n", n2, o2, string(b2))
+
+	o3, err := f.Seek(1, 0)
+	check(err)
+	b3 := make([]byte, 2)
+	n3, err := io.ReadAtLeast(f, b3, 2)
+	check(err)
+	pf("%d bytes @ %d: %s\n", n3, o3, string(b3))
+
+	_, err = f.Seek(0, 0)
+	check(err)
+
+	r4 := bufio.NewReader(f)
+	b4, err := r4.Peek(3)
+	check(err)
+	pf("3 bytes: %s\n", string(b4))
+}

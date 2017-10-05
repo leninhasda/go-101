6d42da808725f595e63820441dd852cdc3d22197
Lenin <lenin@pathao.com>
2017-10-05 13:09:09 +0600

    cli with flags

---
+++ basic/045-cli-flags.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+)
+
+func main() {
+
+	wordPtr := flag.String("word", "foo", "a string")
+
+	numbPtr := flag.Int("numb", 42, "an int")
+	boolPtr := flag.Bool("fork", false, "a bool")
+
+	var svar string
+	flag.StringVar(&svar, "svar", "bar", "a string var")
+
+	flag.Parse()
+
+	fmt.Println("word", *wordPtr)
+	fmt.Println("numb", *numbPtr)
+	fmt.Println("fork", *boolPtr)
+	fmt.Println("svar", svar)
+	// tails are the optional parameters
+	fmt.Println("tail", flag.Args())
+
+}

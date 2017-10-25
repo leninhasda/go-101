67b287683efb238bab31b47c34431de88d178a05
Lenin <lenin@pathao.com>
2017-10-26 01:27:07 +0600

    interface with embedding

--- experiment/003-type-embedding.go
+++ experiment/003-type-embedding.go
@@ -2,14 +2,30 @@ package main
 
 import "fmt"
 
+type notifier interface {
+	notify()
+}
+
 type user struct {
 	name  string
 	email string
 }
 
+func (u *user) notify() {
+	fmt.Printf("sending user email to %s<%s>\n", u.name, u.email)
+}
+
 type admin struct {
 	user
 	level string
+}
+
+type master struct {
+	user
+}
+
+func (m *master) notify() {
+	fmt.Printf("sending master email to %s<%s>\n", m.name, m.email)
 }
 
 func main() {
@@ -30,4 +46,27 @@ func main() {
 	// as the embedded vars will be promoted
 	fmt.Println(ad.name, ad.email)
 
+	u := user{
+		name:  "user",
+		email: "user@mail.com",
+	}
+
+	sendNotification(&u)
+
+	// will work too
+	// because the user.notify will be promoted
+	sendNotification(&ad)
+
+	m := master{
+		user: user{
+			name:  "master",
+			email: "master@mail.com",
+		},
+	}
+
+	sendNotification(&m)
+}
+
+func sendNotification(n notifier) {
+	n.notify()
 }
